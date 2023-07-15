93882dbc382b48a75f8e8bae90b945209ae9e7b4
GyeungHoon <ghp0415@naver.com>
2023-07-16 04:18:56 +0900

    json 수정

--- src/assets/postdata.js
+++ src/assets/postdata.js
@@ -1,12 +1,12 @@
 export default[
   {
-    name: "Kim Hyun",
+    name: "Kim Hyun Woo",
     userImage: "https://picsum.photos/100?random=3",
     postImage: "https://picsum.photos/600?random=3",
     likes: 36,
     date: "May 15",
     liked: false,
-    content: "오늘 무엇을 했냐면요 아무것도 안했어요 ?",
+    content: "오늘은 아무것도 안함",
     filter: "perpetua"
   },
   {
@@ -16,7 +16,7 @@ export default[
     likes: 20,
     date: "Apr 20",
     liked: false,
-    content: "흔한 자랑스타그램",
+    content: "흔한 자랑",
     filter: "clarendon"
   },
   {
@@ -26,7 +26,7 @@ export default[
     likes: 49,
     date: "Apr 4",
     liked: false,
-    content: "우리집 개는 화장실 물도 내림",
+    content: "에헤이~ 조졌네 이거",
     filter: "lofi"
   }
 ]
